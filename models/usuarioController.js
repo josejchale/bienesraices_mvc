35a25eb4898df583c09652de1dd73788f5155143
Jose Chale <chalejosejuan@gmail.com>
2025-06-12 22:39:53 -0600

    feat(views):Vistas de login, sing up, recovery y routing entre vistas

--- models/usuarioController.js
+++ models/usuarioController.js
@@ -1,16 +1,23 @@
 const formularioLogin = (req,res)=>{
         res.render('auth/login',{
-        
+        pagina: 'Iniciar Sesión',
     })
 }
 
 const formularioRegistro = (req,res)=>{
     res.render('auth/registro',{
-        
+        pagina: 'Crear Cuenta'
+    })
+}
+
+const recuperarPassword = (req,res)=>{
+    res.render('auth/recuperar',{
+        pagina: 'Recuperar Contraseña'
     })
 }
 
 export {
     formularioLogin,
-    formularioRegistro
+    formularioRegistro,
+    recuperarPassword
 }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,10 @@
 import  express  from 'express';
-import { formularioLogin, formularioRegistro } from '../models/usuarioController.js';
+import { formularioLogin, formularioRegistro, recuperarPassword } from '../models/usuarioController.js';
 
 const router = express.Router();
 
 router.get("/login", formularioLogin);
 router.get("/registro", formularioRegistro);
+router.get("/recuperar", recuperarPassword)
 
 export default router;
